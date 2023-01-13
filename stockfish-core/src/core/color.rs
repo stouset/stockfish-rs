55570ee37cfa01cd0efa8db718f8a14aff94c397
Stephen Touset <stephen@touset.org>
2023-01-12 18:49:05 -0800

    Additional methods on Color

--- stockfish-core/src/core/color.rs
+++ stockfish-core/src/core/color.rs
@@ -1,5 +1,7 @@
 use crate::prelude::*;
 
+use std::ops::Not;
+
 enumeration! {
     /// A color of a chess piece, white or black.
     pub Color, [ White, Black ]
@@ -8,10 +10,56 @@ enumeration! {
 impl Color {
     const RANKS: [Rank; Self::COUNT] = [ Rank::_1, Rank::_8 ];
 
+    #[inline]
+    #[must_use]
+    pub fn is_white(self) -> bool {
+        self == Self::White
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn is_black(self) -> bool {
+        self == Self::Black
+    }
+
     /// The "home" [`Rank`] for the color. [`Rank::_1`] for [`Color::White`],
     /// [`Rank::_8`] for [`Color::Black`].
     #[inline]
     pub const fn rank(self) -> Rank {
         Self::RANKS[self]
+    }
+
+    /// Returns the direction the [`Color`]'s pawns move.
+    #[inline]
+    pub const fn direction(self) -> Direction {
+        if self == Self::White { Direction::N } else { Direction::S }
+    }
+}
+
+impl const Not for Color {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        unsafe_optimization!(
+            Self::from_u8(self.as_u8() ^ 1).unwrap(),
+            Self::from_u8_unchecked(self.as_u8() ^ 1),
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn direction() {
+        assert_eq!(Direction::N, Color::White.direction());
+        assert_eq!(Direction::S, Color::Black.direction());
+    }
+
+    #[test]
+    fn not() {
+        assert_eq!(Color::Black, !Color::White);
+        assert_eq!(Color::White, !Color::Black);
     }
 }
