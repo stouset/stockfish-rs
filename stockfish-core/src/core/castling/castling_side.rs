422f85a310c1f32a2489b9ef829b70b8de0beea8
Stephen Touset <stephen@touset.org>
2023-01-12 19:36:54 -0800

    Castling-related tests

--- stockfish-core/src/core/castling/castling_side.rs
+++ stockfish-core/src/core/castling/castling_side.rs
@@ -13,3 +13,16 @@ impl CastlingSide {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn detect() {
+        assert_eq!(CastlingSide::Kingside, CastlingSide::detect(File::_F, File::_G));
+        assert_eq!(CastlingSide::Kingside, CastlingSide::detect(File::_A, File::_B));
+
+        assert_eq!(CastlingSide::Queenside, CastlingSide::detect(File::_H, File::_C));
+        assert_eq!(CastlingSide::Queenside, CastlingSide::detect(File::_F, File::_E));
+    }
+}
--- stockfish-core/src/core/castling/castling_variety.rs
+++ stockfish-core/src/core/castling/castling_variety.rs
@@ -55,3 +55,41 @@ impl CastlingVariety {
         Self::RIGHTS[self]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn color() {
+        assert_eq!(Color::White, CastlingVariety::WhiteKingside .color());
+        assert_eq!(Color::White, CastlingVariety::WhiteQueenside.color());
+        assert_eq!(Color::Black, CastlingVariety::BlackKingside .color());
+        assert_eq!(Color::Black, CastlingVariety::BlackQueenside.color());
+    }
+
+
+    #[test]
+    fn king_destination() {
+        assert_eq!(Square::G1, CastlingVariety::WhiteKingside .king_destination());
+        assert_eq!(Square::C1, CastlingVariety::WhiteQueenside.king_destination());
+        assert_eq!(Square::G8, CastlingVariety::BlackKingside .king_destination());
+        assert_eq!(Square::C8, CastlingVariety::BlackQueenside.king_destination());
+    }
+
+    #[test]
+    fn rook_destination() {
+        assert_eq!(Square::F1, CastlingVariety::WhiteKingside .rook_destination());
+        assert_eq!(Square::D1, CastlingVariety::WhiteQueenside.rook_destination());
+        assert_eq!(Square::F8, CastlingVariety::BlackKingside .rook_destination());
+        assert_eq!(Square::D8, CastlingVariety::BlackQueenside.rook_destination());
+    }
+
+    #[test]
+    fn rights() {
+        assert_eq!(CastlingRights::WHITE_OO,  CastlingVariety::WhiteKingside .rights());
+        assert_eq!(CastlingRights::WHITE_OOO, CastlingVariety::WhiteQueenside.rights());
+        assert_eq!(CastlingRights::BLACK_OO,  CastlingVariety::BlackKingside .rights());
+        assert_eq!(CastlingRights::BLACK_OOO, CastlingVariety::BlackQueenside.rights());
+    }
+}
